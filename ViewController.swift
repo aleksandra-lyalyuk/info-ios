7e2de04c480b9278afe39f123754d439c5f27358
aleksandra-lyalyuk <64406400+aleksandra-lyalyuk@users.noreply.github.com>
2020-04-28 20:08:41 +0300

    add refresh

--- ViewController.swift
+++ ViewController.swift
@@ -10,14 +10,44 @@ import UIKit
 import WebKit
 import SafariServices
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate{
+    @IBOutlet var Activity: UIActivityIndicatorView! 
     @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         let request = URLRequest(url: URL(string: "http://lk.tatgencom.ru")!)
         webView.load(request)
+        let button = UIButton()
+        button.setImage(UIImage(systemName:"arrow.clockwise")!, for: .normal)
+        button.addTarget(self, action: #selector(reload), for: .touchUpInside)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: button)
+        webView.navigationDelegate = self
+        
+        // add activity
+        self.Activity.startAnimating()
+        self.webView.navigationDelegate = self
+        self.Activity.hidesWhenStopped = true
+        
+    }
+    
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        Activity.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        Activity.stopAnimating()
+    }
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        Activity.startAnimating()
+    }
+    @objc func reload(){
+        
+        webView.load(URLRequest(url: URL(string:"about:blank")!))
+        Activity.startAnimating()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+        let request = URLRequest(url: URL(string: "http://lk.tatgencom.ru")!)
+    self.webView.load(request)
+        }
     }
 }
-
